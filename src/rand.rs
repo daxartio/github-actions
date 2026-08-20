1c8b4c894b24a078cbe50146ff03d16a01f33cb1
Danil Akhtarov <daxartio@gmail.com>
2026-08-20 16:50:28 +0400

    refactor(rand): remove uuid dependency

--- src/rand.rs
+++ src/rand.rs
@@ -1,7 +1,5 @@
-#[cfg(not(feature = "uuid"))]
 use std::time::{SystemTime, UNIX_EPOCH};
 
-#[cfg(not(feature = "uuid"))]
 pub(crate) fn random_id() -> String {
     let now = SystemTime::now()
         .duration_since(UNIX_EPOCH)
@@ -11,17 +9,6 @@ pub(crate) fn random_id() -> String {
     format!("{:x}-{:x}", now, addr)
 }
 
-#[cfg(not(feature = "uuid"))]
-pub(crate) fn delimiter() -> String {
-    format!("ghadelimiter_{}", random_id())
-}
-
-#[cfg(feature = "uuid")]
-pub(crate) fn random_id() -> String {
-    uuid::Uuid::new_v4().to_string()
-}
-
-#[cfg(feature = "uuid")]
 pub(crate) fn delimiter() -> String {
     format!("ghadelimiter_{}", random_id())
 }
